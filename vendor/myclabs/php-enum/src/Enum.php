a7d44a9ef113085b96342e5cbaea40f56a2bd107
KingFer <kingfer30@qq.com>
2023-07-31 00:19:31 +0800

    升级PHP8.0

--- vendor/myclabs/php-enum/src/Enum.php
+++ vendor/myclabs/php-enum/src/Enum.php
@@ -197,6 +197,7 @@ abstract class Enum implements \JsonSerializable
      * @return mixed
      * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
      */
+    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return $this->getValue();
